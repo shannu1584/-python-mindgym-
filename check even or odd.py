a424553e15e96f7ee1ef95e37ee64a5fe097a1e9
Karri Shanmukha Pradeepthi <145190190+shannu1584@users.noreply.github.com>
2025-07-28 14:38:51 +0530

    Add files via upload

---
+++ check even or odd.py
@@ -0,0 +1,13 @@
+n=int(input())
+if n%2==0:
+    print(f"{n} is even number")
+else:
+    print(f"{n} is odd number")
+
+def evenorodd(n):
+    if n%2==0:
+        print("even")
+    else:
+        print("odd")
+t=int(input())
+evenorodd(t)      
