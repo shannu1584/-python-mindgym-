999a0fc9bee497cc6931143a8a20572d98defeda
Karri Shanmukha Pradeepthi <145190190+shannu1584@users.noreply.github.com>
2025-07-28 14:26:56 +0530

    Add files via upload

---
+++ max of two numbers.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=int(input())
+print("Maximum of two numbers:",max(a,b))
+
+def maximum(a,b):
+    return max(a,b)
+n1=int(input())
+n2=int(input())
+result=maximum(n1,n2)
+print(result)
